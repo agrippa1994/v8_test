c4c5cfc15acebff9bc78ff85f71e3cdd2badf325
agrippa1994 <agrippa1994@icloud.com>
2014-10-14 18:26:09 +0200

    Some updates

--- v8_test/main.cpp
+++ v8_test/main.cpp
@@ -1,3 +1,6 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include <v8/v8.h>
 #include <v8/v8-platform.h>
 #include <v8/libplatform/libplatform.h>
@@ -6,6 +9,7 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <thread>
 
 #pragma comment(lib, "winmm.lib")
 #pragma comment(lib, "icui18n.lib")
@@ -44,7 +48,7 @@ void print(const FunctionCallbackInfo<Value>& args)
 
 void callNTimes(const FunctionCallbackInfo<Value>& args)
 {
-	auto isolate = Isolate::GetCurrent();
+	auto isolate = args.GetIsolate();
 	HandleScope scope(isolate);
 
 	if (args.Length() != 2)
@@ -62,18 +66,152 @@ void callNTimes(const FunctionCallbackInfo<Value>& args)
 
 	Handle<Function> func = Handle<Function>::Cast(args[1]);
 
-
 	for (int i = 0; i < args[0]->ToNumber()->Int32Value(); i++)
 		func->Call(isolate->GetCurrentContext()->Global(), 0, {});
-
 }
 
+void sleep(const FunctionCallbackInfo<Value>& args)
+{
+	auto isolate = args.GetIsolate();
+	HandleScope scope(isolate);
+
+	if (args.Length() != 1)
+	{
+		isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter count!"));
+		return;
+	}
+
+	if (!args[0]->IsNumber())
+	{
+		isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter types!"));
+		return;
+	}
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(args[0]->ToNumber()->Int32Value()));
+}
+
+struct TestObject
+{
+	int x = 0;
+	int y = 0;
+
+	static void get(Local<String> property, const PropertyCallbackInfo<Value>& info) 
+	{
+		auto isolate = info.GetIsolate();
+		HandleScope scope(isolate);
+
+		TestObject *object = (TestObject *) Local<External>::Cast(info.Holder()->GetInternalField(0))->Value();
+
+		std::string szProperty = *String::Utf8Value(property);
+
+		if (szProperty == "x") info.GetReturnValue().Set(object->x);
+		if (szProperty == "y") info.GetReturnValue().Set(object->y);
+	}
+
+	static void set(Local<String> property, Local<Value> value, const PropertyCallbackInfo<Value>& info)
+	{
+		auto isolate = info.GetIsolate();
+		HandleScope scope(isolate);
+
+		TestObject *object = (TestObject *) Local<External>::Cast(info.Holder()->GetInternalField(0))->Value();
+
+		std::string szProperty = *String::Utf8Value(property);
+
+		if (!value->IsNumber())
+			return;
+
+		if (szProperty == "x") object->x = value->ToNumber()->Int32Value();
+		if (szProperty == "y") object->y = value->ToNumber()->Int32Value();
+	}
+
+	static void sum(const FunctionCallbackInfo<Value>& args)
+	{
+		auto isolate = args.GetIsolate();
+		HandleScope scope(isolate);
+
+		TestObject *object = (TestObject *) Local<External>::Cast(args.This()->GetInternalField(0))->Value();
+
+		args.GetReturnValue().Set(object->x + object->y);
+	}
+
+	static void CreateTestObject(const FunctionCallbackInfo<Value>& args)
+	{
+		auto isolate = args.GetIsolate();
+		HandleScope scope(isolate);
+
+		if (args.Length() != 2)
+		{
+			isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter count!"));
+			return;
+		}
+
+		if (!args[0]->IsNumber() || !args[1]->IsNumber())
+		{
+			isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter types!"));
+			return;
+		}
+		
+		TestObject *pThis = new TestObject;
+
+		pThis->x = args[0]->ToNumber()->Int32Value();
+		pThis->y = args[1]->ToNumber()->Int32Value();
+
+
+		static Persistent<ObjectTemplate> objectTemplateHeap;
+		if (objectTemplateHeap.IsEmpty())
+		{
+			auto objectTemplate = ObjectTemplate::New(isolate);
+
+			objectTemplate->SetInternalFieldCount(1);
+			objectTemplate->SetNamedPropertyHandler(TestObject::get, TestObject::set);
+			objectTemplate->Set(String::NewFromUtf8(isolate, "sum"), FunctionTemplate::New(isolate, TestObject::sum));
+
+			objectTemplateHeap.Reset(isolate, objectTemplate);
+		}
+
+		auto objectTemplate = Local<ObjectTemplate>::New(isolate, objectTemplateHeap);
+		
+		auto object = objectTemplate->NewInstance();
+		object->SetInternalField(0, External::New(isolate, pThis));
+
+		args.GetReturnValue().Set(object);
+	}
+
+	static void DeleteTestObject(const FunctionCallbackInfo<Value>& args)
+	{
+		auto isolate = args.GetIsolate();
+		HandleScope scope(isolate);
+
+		if (args.Length() != 1)
+		{
+			isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter count!"));
+			return;
+		}
+
+		if (!args[0]->IsObject())
+		{
+			isolate->ThrowException(String::NewFromUtf8(isolate, "Wrong parameter types!"));
+			return;
+		}
+
+		auto internal = Local<External>::Cast(args[0]->ToObject()->GetInternalField(0));
+		if (internal->Value() != NULL)
+		{
+			delete (TestObject *)internal->Value();
+			args[0]->ToObject()->SetInternalField(0, External::New(isolate, 0));
+		}
+	}
+};
 Handle<Context> InitializeJS(Isolate *isolate)
 {
 	Handle<ObjectTemplate> global = ObjectTemplate::New(isolate);
-
+	
 	global->Set(isolate, "print", FunctionTemplate::New(isolate, print));
 	global->Set(isolate, "callNTimes", FunctionTemplate::New(isolate, callNTimes));
+	global->Set(isolate, "sleep", FunctionTemplate::New(isolate, sleep));
+	global->Set(isolate, "CreateTestObject", FunctionTemplate::New(isolate, TestObject::CreateTestObject));
+	global->Set(isolate, "DeleteTestObject", FunctionTemplate::New(isolate, TestObject::DeleteTestObject));
+	global->Set(isolate, "GetTickCount", FunctionTemplate::New(isolate, [](const FunctionCallbackInfo<Value>& args) { args.GetReturnValue().Set(Number::New(args.GetIsolate(), GetTickCount())); }));
 
 	return Context::New(isolate, NULL, global);
 }
@@ -104,8 +242,10 @@ int ReportError(Isolate *isolate, TryCatch *try_catch = 0)
 
 int main(int argc, char* argv [])
 {
+	V8::InitializeICU();
+	V8::InitializePlatform(v8::platform::CreateDefaultPlatform());
 	V8::Initialize();
-
+	
 	Isolate* isolate = Isolate::New();
 	{
 		Isolate::Scope isolate_scope(isolate);
@@ -124,6 +264,8 @@ int main(int argc, char* argv [])
 		auto run = script->Run();
 		if (run.IsEmpty())
 			return ReportError(isolate, &try_catch);
+
+		std::cout << "Finished!" << std::endl;
 	}
 
 	return ReportError(isolate, 0);
--- v8_test/main.js
+++ v8_test/main.js
@@ -1,21 +1,54 @@
-print("asdf")
 
-print(JSON.stringify({id: 0}));
+// Test the garbage collector
+function testGarbageCollector(time)
+{
+	if(typeof time != "number")
+		return false
 
-callNTimes(5, function(){
-	print("Callback")
-})
+	for(var startTick = GetTickCount(); GetTickCount() < startTick + time;) {
+		var obj = new CreateTestObject(5, 5)
 
-try {
-	callNTimes()
-}
-catch(error) {
-	print(error)
+		obj.x = 12
+		var sum = obj.sum()
+
+		DeleteTestObject(obj)
+		delete obj
+	}
+
+	return true
 }
 
-try {
-	callNTimes(5, 6)
+function testCallNTimes(times)
+{
+	var i=0
+
+	callNTimes(times, function() { 
+		i++
+	})
+
+	return times == i
 }
-catch(error) {
-	print(error)
-}
+
+print("V8-Tests will be started...\n")
+
+print("The garbage collector will be tested...")
+testGarbageCollector(5000)
+print("The garbage collector has been successfully tested.")
+
+print("\n")
+
+print("CallNTimes will be tested...")
+var res = testCallNTimes(10000)
+if(res)
+	print("CallNTimes has been successfully tested")
+else
+	print("CallNTimes failed!")
+
+print("\n")
+
+print("Test sleep of one second (TickCount : " + GetTickCount() + ")")
+sleep(1000)
+print("Test sleep finished (TickCount: " + GetTickCount() + ")")
+
+print("\n")
+print("V8-Tests finished")
